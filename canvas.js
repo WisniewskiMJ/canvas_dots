70c965bc7e8ad35b428423f784bce23ff958da0b
Bakshee <bakshee.jw@gmail.com>
2019-03-19 01:34:49 +0100

    added random coloring and mouse interaction (mousemove

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-let canvas = document.querySelector('canvas');
+ let canvas = document.querySelector('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -10,17 +10,39 @@ let x = Math.floor(Math.random() * ((innerWidth - r) - (0  + r))) + (0 + r);
 //let dx = (Math.random() - 0.5)*20;
 let y = Math.floor(Math.random() * ((innerHeight - r) - (0  +r))) + (0 + r);
 //let dy = (Math.random() - 0.5)*20;
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+let colorArray = [
+    '#375e7f', 
+    '#3a383d',
+    '#805d4a',
+    '#8d8bbc',
+    '#f7c705'
+ ];
 
-function Circle(x, y) {
-	this.r = Math.floor(Math.random()*(50-5)) + 5;
+
+window.addEventListener('mousemove', 
+                        function(event) {
+                            mouse.x = event.x;
+                            mouse.y = event.y;
+                        }
+                       )
+//console.log(mouse.x);
+function Circle(x, y, r) {
+     
+	this.r = r;
+    this.rMin = r;
+    this.rMax = 40;
     this.x = x;
 	this.y = y; 
-    
+    this.color = colorArray[Math.floor(Math.random()*colorArray.length)];
     // using modulo 
 	// this.dx = Math.floor(((Math.random()*100)%20)+5);
     
-    this.dx = Math.floor(Math.random()*(15+15))-15;
-	this.dy = Math.floor(Math.random()*(15+15))-15;
+    this.dx = (Math.random()-0.5)*3;
+	this.dy = (Math.random()-0.5)*3;
 	
 	this.alpha = 1;
     this.beta = 1;
@@ -30,9 +52,9 @@ function Circle(x, y) {
 		c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
 		c.strokeStyle = `rgba(0 ,0 ,255 , ${this.alpha})`;
 		c.lineWidth = 4;
-        //c.fillStyle = `rgba(255, 255, 255, ${this.beta})`;
-		c.stroke();
-        //c.fill();
+        c.fillStyle = this.color;  //`rgba(55 ,94 ,127 , ${this.beta})`;
+		//c.stroke();
+        c.fill();
 	};
 	this.update = function() {
 		this.x += this.dx;
@@ -46,14 +68,25 @@ function Circle(x, y) {
         //this.alpha -= 0.005;
         //this.beta -= 0.005;
         //this.r += 0.01;
+        //console.log(mouse.x);
+        
+        if (mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+            mouse.y - this.y < 50 && mouse.y - this.y > -50 &&
+            this.r < this.rMax) {
+            this.r += 2;    
+        } else if (this.r > this.rMin) {
+            this.r -= 1;
+        }
+        
         this.draw();
 	}
 };
 
 let circleArray = [];
 
-for (i = 0; i < 25; i++) {
-    circleArray.push(new Circle(x,y));
+for (i = 0; i < 800; i++) {
+    let r = Math.floor(Math.random() * 7) + 2;
+    circleArray.push(new Circle(x,y,r));
 } 
 
 function animate () {
@@ -62,6 +95,7 @@ function animate () {
 	 
     for (let i = 0; i < circleArray.length; i++) {
         circleArray[i].update();
+        
     }
 }
 
